dbd84cd73a83f0848ea5b09f3185abfc6d618a62
Adriel Nobre <adrielnobre@rede.ulbra.br>
2022-05-30 14:18:25 -0300

    test IDE

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        testeSecond teste = new testeSecond(5,10);
+
+        System.out.println(teste.calculaAB());
+
+
+    }
+}
